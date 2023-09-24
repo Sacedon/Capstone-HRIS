eb17a3c55745b7bd6aec8928dec5073c6c8afdd6
Sacedon <christiansacedon3@gmail.com>
2023-09-25 00:21:33 +0800

    change-design

--- resources/views/components/header.blade.php
+++ resources/views/components/header.blade.php
@@ -5,7 +5,7 @@
             <div class="flex items-center">
                 <a href="{{ route('profile-show') }}" class="flex items-center space-x-2">
                     <div class="w-12 h-12 overflow-hidden rounded-full">
-                        <img src="{{ Auth::user()->profile_picture ? Storage::url(Auth::user()->profile_picture) : asset('images/mdc-logo.jpg') }}"
+                        <img src="{{ Auth::user()->profile_picture ? Storage::url(Auth::user()->profile_picture) : asset('images/default-profile.jpeg') }}"
                             alt="Profile Picture" class="w-full h-full object-cover" />
                     </div>
 
